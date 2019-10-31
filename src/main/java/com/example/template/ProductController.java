33131bcd4f5b1739aa24c92bd3b1613bc60975ed
ymlee123 <57166377+ymlee123@users.noreply.github.com>
2019-10-31 17:23:16 +0900

    Update ProductController.java

--- src/main/java/com/example/template/ProductController.java
+++ src/main/java/com/example/template/ProductController.java
@@ -11,7 +11,7 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @GetMapping("/product/{productId}")
+    @GetMapping("/produc/{productId}")
     Product productStockCheck(@PathVariable(value = "productId") Long productId) {
         return  this.productService.getProductById(productId);
     }
